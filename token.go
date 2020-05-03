3c8f258319bc4273640f4c285c2fc7df75c04d8d
Pete Fraedrich <pete@taskfit.io>
2020-05-03 17:11:46 -0400

    add metrics to tokenstore

--- token.go
+++ token.go
@@ -7,6 +7,8 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 // TokenStore manages cluster tokens
@@ -31,22 +33,97 @@ func NewTokenStore(app *Cave) (*TokenStore, error) {
 		tokens:    app.tokens,
 		metrics:   tokenMetrics(),
 		lock:      sync.Mutex{},
+		store:     map[string]Token{},
 	}
 	return t, nil
 }
 
 func tokenMetrics() map[string]interface{} {
-	return map[string]interface{}{}
+	return map[string]interface{}{
+		"token_count": promauto.NewGauge(prometheus.GaugeOpts{
+			Name: "cave_tokens_token_count",
+			Help: "The number of tokens in the token store",
+		}),
+		"token_detail": promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "cave_tokens_token_details",
+			Help: "Tokens broken down by details",
+		}, []string{"issue_time", "expire_time", "user", "issued_by", "type", "token_redacted"}),
+		"tokens_issued": promauto.NewCounter(prometheus.CounterOpts{
+			Name: "cave_tokens_issued_count",
+			Help: "Number of issued tokens",
+		}),
+		"tokens_issued_detail": promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "cave_tokens_issue_detail",
+			Help: "Tokens issued, detail view",
+		}, []string{"issue_time", "expire_time", "user", "issued_by", "type", "token_redacted"}),
+		"tokens_deleted": promauto.NewCounter(prometheus.CounterOpts{
+			Name: "cave_tokens_deleted_count",
+			Help: "Number of tokens deleted",
+		}),
+		"token_cleanup": promauto.NewGauge(prometheus.GaugeOpts{
+			Name: "cave_tokens_cleanup_cycle_duration",
+			Help: "Time taken for cleaning up tokens",
+		}),
+		"token_cleanup_count": promauto.NewGauge(prometheus.GaugeOpts{
+			Name: "cave_tokens_cleanup_cycle_count",
+			Help: "Number of tokens cleaned during cycle",
+		}),
+		"token_cleanup_run_count": promauto.NewCounter(prometheus.CounterOpts{
+			Name: "cave_tokens_cleanup_cycle_run_count",
+			Help: "Number of times the cleanup cycle has run",
+		}),
+		"timings": promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "cave_tokens_transaction_times_ms",
+			Help: "Transaction times for token functions in ms",
+		}, []string{"function"}),
+		"update_queue": promauto.NewGauge(prometheus.GaugeOpts{
+			Name: "cave_tokens_update_queue_len",
+			Help: "Length of the update queue",
+		}),
+	}
 }
 
 // Start function
 func (t *TokenStore) Start() {
+	idx := 0
 	for {
+		idx++
+		go func() {
+			t.metrics["update_queue"].(prometheus.Gauge).Set(float64(len(t.tokens)))
+			t.metrics["token_count"].(prometheus.Gauge).Set(float64(len(t.store)))
+			for _, v := range t.store {
+				t.metrics["token_detail"].(*prometheus.GaugeVec).WithLabelValues(v.IssueTime.String(), v.ExpireTime.String(), v.UID, v.IssuedBy, v.Type, fmt.Sprintf("xxxx-%s", v.Token[24:])).Set(1.0)
+			}
+		}()
 		select {
 		case m := <-t.tokens:
 			err := t.HandleUpdate(m)
 			if err != nil {
 				t.log.Error(err)
+			}
+		default:
+			// clean up tokens every 100 runs or so
+			if idx%100 == 0 {
+				start := time.Now()
+				idx = 0
+				del := []string{}
+				for k, v := range t.store {
+					if time.Now().Sub(v.IssueTime) <= 0 || time.Now().Sub(v.ExpireTime) > 0 {
+						del = append(del, k)
+					}
+				}
+				for _, i := range del {
+					err := t.Delete(i)
+					if err != nil {
+						t.log.Error(err)
+					}
+				}
+				go func() {
+					t.metrics["token_cleanup"].(prometheus.Gauge).Set(float64(time.Now().Sub(start).Milliseconds()))
+					t.metrics["token_cleanup_count"].(prometheus.Gauge).Set(float64(len(del)))
+					t.metrics["token_cleanup_run_count"].(prometheus.Counter).Inc()
+
+				}()
 			}
 		}
 		time.Sleep(100 * time.Millisecond)
@@ -55,6 +132,10 @@ func (t *TokenStore) Start() {
 
 // HandleUpdate func
 func (t *TokenStore) HandleUpdate(m Message) error {
+	start := time.Now()
+	defer func() {
+		t.metrics["timings"].(*prometheus.GaugeVec).WithLabelValues("handle_update").Set(float64(time.Now().Sub(start).Milliseconds()))
+	}()
 	var d map[string]Token
 	err := json.Unmarshal(m.Data, &d)
 	if err != nil {
@@ -68,6 +149,10 @@ func (t *TokenStore) HandleUpdate(m Message) error {
 
 // Find function
 func (t *TokenStore) Find(tid string) (Token, error) {
+	start := time.Now()
+	defer func() {
+		t.metrics["timings"].(*prometheus.GaugeVec).WithLabelValues("find").Set(float64(time.Now().Sub(start).Milliseconds()))
+	}()
 	if val, ok := t.store[tid]; ok {
 		return val, nil
 	}
@@ -76,6 +161,10 @@ func (t *TokenStore) Find(tid string) (Token, error) {
 
 // Validate function
 func (t *TokenStore) Validate(typ string, uid string, tid string) bool {
+	start := time.Now()
+	defer func() {
+		t.metrics["timings"].(*prometheus.GaugeVec).WithLabelValues("validate").Set(float64(time.Now().Sub(start).Milliseconds()))
+	}()
 	token, err := t.Find(tid)
 	if err != nil {
 		return false
@@ -88,6 +177,10 @@ func (t *TokenStore) Validate(typ string, uid string, tid string) bool {
 
 // FindUser function
 func (t *TokenStore) FindUser(uid string) (Token, error) {
+	start := time.Now()
+	defer func() {
+		t.metrics["timings"].(*prometheus.GaugeVec).WithLabelValues("find_user").Set(float64(time.Now().Sub(start).Milliseconds()))
+	}()
 	for _, v := range t.store {
 		if v.UID == uid {
 			return v, nil
@@ -98,6 +191,10 @@ func (t *TokenStore) FindUser(uid string) (Token, error) {
 
 // Delete function
 func (t *TokenStore) Delete(tid string) error {
+	start := time.Now()
+	defer func() {
+		t.metrics["timings"].(*prometheus.GaugeVec).WithLabelValues("delete").Set(float64(time.Now().Sub(start).Milliseconds()))
+	}()
 	if _, ok := t.store[tid]; ok {
 		t.lock.Lock()
 		delete(t.store, tid)
@@ -108,11 +205,16 @@ func (t *TokenStore) Delete(tid string) error {
 	if err != nil {
 		return err
 	}
+	go t.metrics["tokens_deleted"].(prometheus.Counter).Inc()
 	return fmt.Errorf("Token does not exist")
 }
 
 // Issue function
 func (t *TokenStore) Issue(user string, typ string, expire ...bool) (Token, error) {
+	start := time.Now()
+	defer func() {
+		t.metrics["timings"].(*prometheus.GaugeVec).WithLabelValues("issue").Set(float64(time.Now().Sub(start).Milliseconds()))
+	}()
 	e := time.Now().Add(60 * time.Minute)
 	if len(expire) > 0 && expire[0] == false {
 		e = time.Now().Add(87600 * time.Hour)
@@ -123,7 +225,7 @@ func (t *TokenStore) Issue(user string, typ string, expire ...bool) (Token, erro
 		ExpireTime: e,
 		UID:        user,
 		Type:       typ,
-		IssuedBy:   t.app.Cluster.node.Addr(),
+		IssuedBy:   t.app.Crypto.id,
 		Token:      token,
 	}
 	t.lock.Lock()
@@ -133,15 +235,27 @@ func (t *TokenStore) Issue(user string, typ string, expire ...bool) (Token, erro
 	if err != nil {
 		return tok, err
 	}
+	go func() {
+		t.metrics["tokens_issued"].(prometheus.Counter).Inc()
+		t.metrics["tokens_issued_detail"].(*prometheus.GaugeVec).WithLabelValues(tok.IssueTime.String(), tok.ExpireTime.String(), tok.UID, tok.IssuedBy, tok.Type, fmt.Sprintf("xxxx-%s", tok.Token[24:])).Set(1.0)
+	}()
 	return tok, nil
 }
 
 func (t *TokenStore) emit() error {
+	start := time.Now()
+	defer func() {
+		t.metrics["timings"].(*prometheus.GaugeVec).WithLabelValues("emit").Set(float64(time.Now().Sub(start).Milliseconds()))
+	}()
+	if t.config.Mode == "dev" {
+		return nil
+	}
 	t.lock.Lock()
 	b, err := json.Marshal(t.store)
 	if err != nil {
 		return err
 	}
+	t.lock.Unlock()
 	err = t.app.Cluster.Emit("token", b, "token:update")
 	if err != nil {
 		return err
